ceae81d2c45b81fc504b552ca3c663317f14203d
MichaelLee826 <michaelleef2008@gmail.com>
2019-01-05 09:20:22 +0800

    增加了对y轴的注释

--- Chapter2/chapter2-2.py
+++ Chapter2/chapter2-2.py
@@ -5,12 +5,18 @@ import matplotlib.pyplot as plt
 mpl.rcParams["font.sans-serif"] = ["SimHei"]
 mpl.rcParams["axes.unicode_minus"] = False
 
+# 指定每个bin(箱子)分布的数据，对应x轴
 x = np.random.randint(0, 10, 100)
+print(x)
+# y轴的值表示对应的x的值出现的频数（频率），例如100个数中有10个5，则5对应的y为10（0.1）
 
+# 这个参数指定bin(箱子)的个数，也就是总共有几条条状图
 bins = range(0, 11, 1)
 
 # 直方图
-plt.hist(x, bins=bins, color="g", histtype="bar", rwidth=0.8, alpha=0.6)
+plt.hist(x, bins=bins, density=False, color="g", histtype="bar", rwidth=0.8, alpha=0.6)
+# density=True      y轴表示的是频率
+# density=False     y轴表示的是频数
 
 plt.xlabel("箱子重量（kg）")
 plt.ylabel("销售数量（个）")
